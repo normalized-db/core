d27bde08c44bdea89b2632ce80db29fe31cc1d6c
Sandro Schmid <saseb.schmid@gmail.com>
2017-09-20 16:45:28 +0200

    Revert "add range"
    This reverts commit 629a69b

--- src/model/index.ts
+++ src/model/index.ts
@@ -3,7 +3,6 @@ import { FetchCallback } from './fetch-callback';
 import { KeyMap } from './key-map';
 import { NormalizedData } from './normalized-data';
 import { Parent } from './parent';
-import { Range } from './range';
 import { UniqueKeyCallback } from './unique-key-callback';
 import { ValidKey } from './valid-key';
 
@@ -13,7 +12,6 @@ export {
   KeyMap,
   NormalizedData,
   Parent,
-  Range,
   UniqueKeyCallback,
   ValidKey
 };
