34ad2349435e6dc27fea5c04a1d3828386dc0164
Sandro Schmid <saseb.schmid@gmail.com>
2017-11-30 17:35:42 +0100

    add refs-utils

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,10 +1,12 @@
 import { deepClone } from './deep-clone';
 import { isNull, isObject } from './object';
 import { Queue } from './queue';
+import { RefsUtility } from './refs';
 
 export {
   isNull,
   isObject,
   deepClone,
-  Queue
+  Queue,
+  RefsUtility
 };
---
+++ src/utility/refs.ts
@@ -0,0 +1,28 @@
+import { ValidKey } from '../model/valid-key';
+
+export class RefsUtility {
+
+  public static has(item: any, type: string) {
+    return '_refs' in item && type in item._refs && item._refs.type.size > 0;
+  }
+
+  public static hasKey(item: any, type: string, key: ValidKey) {
+    return this.has(item, type) && item._refs.type.has(key);
+  }
+
+  public static iterator(item: any, type: string): Iterator<ValidKey> {
+    return this.has(item, type) ? item._refs.item.values() : {
+      next: () => {
+        return { done: true };
+      }
+    };
+  }
+
+  public static getAll<Key extends ValidKey>(item: any, type: string): ValidKey[] {
+    return this.has(item, type) ? Array.from(item._refs.item) : [];
+  }
+
+  public static first<Key extends ValidKey>(item: any, type: string): ValidKey | null {
+    return this.has(item, type) ? item._refs.item.values().next().value : null;
+  }
+}
