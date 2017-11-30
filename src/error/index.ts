d2c2e18e79099ed2644c22fff2df46b734d6483a
Sandro Schmid <saseb.schmid@gmail.com>
2017-11-30 20:45:44 +0100

    add common errors

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,5 +1,11 @@
 import { InvalidTypeError } from './invalid-type-error';
+import { MissingKeyError } from './missing-key-error';
+import { NotFoundError } from './not-found-error';
+import { TypeMismatchError } from './type-mismatch-error';
 
 export {
-  InvalidTypeError
+  InvalidTypeError,
+  MissingKeyError,
+  NotFoundError,
+  TypeMismatchError
 };
