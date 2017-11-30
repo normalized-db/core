1fd3556bed21942031e19ac6eb472ad3a81a193e
Sandro Schmid <saseb.schmid@gmail.com>
2017-11-30 20:17:36 +0100

    remove `Parent`

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,7 +2,6 @@ import { Depth } from './depth';
 import { FetchCallback } from './fetch-callback';
 import { KeyMap } from './key-map';
 import { NormalizedData } from './normalized-data';
-import { Parent } from './parent';
 import { UniqueKeyCallback } from './unique-key-callback';
 import { ValidKey } from './valid-key';
 
@@ -11,7 +10,6 @@ export {
   FetchCallback,
   KeyMap,
   NormalizedData,
-  Parent,
   UniqueKeyCallback,
   ValidKey
 };
