35645dd5a855c5d5fc67b4e809fa6db7e721a046
Sandro Schmid <saseb.schmid@gmail.com>
2017-12-01 05:41:35 +0100

    minor fixes

--- src/utility/refs.ts
+++ src/utility/refs.ts
@@ -3,15 +3,15 @@ import { ValidKey } from '../model/valid-key';
 export class RefsUtility {
 
   public static has(item: any, type: string) {
-    return '_refs' in item && type in item._refs && item._refs.type.size > 0;
+    return '_refs' in item && type in item._refs && item._refs[type].size > 0;
   }
 
   public static hasKey(item: any, type: string, key: ValidKey) {
-    return this.has(item, type) && item._refs.type.has(key);
+    return this.has(item, type) && item._refs[type].has(key);
   }
 
   public static iterator(item: any, type: string): Iterator<ValidKey> {
-    return this.has(item, type) ? item._refs.item.values() : {
+    return this.has(item, type) ? item._refs[type].values() : {
       next: () => {
         return { done: true };
       }
@@ -19,10 +19,10 @@ export class RefsUtility {
   }
 
   public static getAll<Key extends ValidKey>(item: any, type: string): ValidKey[] {
-    return this.has(item, type) ? Array.from(item._refs.item) : [];
+    return this.has(item, type) ? Array.from(item._refs[type]) : [];
   }
 
   public static first<Key extends ValidKey>(item: any, type: string): ValidKey | null {
-    return this.has(item, type) ? item._refs.item.values().next().value : null;
+    return this.has(item, type) ? item._refs[type].values().next().value : null;
   }
 }
