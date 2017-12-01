7b3212f77e7adc7d1de60c6903b1588d0efb50e9
Sandro Schmid <saseb.schmid@gmail.com>
2017-12-01 05:41:35 +0100

    minor fixes

--- src/error/missing-key-error.ts
+++ src/error/missing-key-error.ts
@@ -1,6 +1,6 @@
 export class MissingKeyError extends Error {
 
   constructor(type: string, keyField: string) {
-    super(`Key is missing in object of type ${type} (key-field ${keyField}`);
+    super(`Key ${keyField} is missing in object of type ${type}`);
   }
 }
