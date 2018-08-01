a38726bacd0a4fddffe9608c4c28f134f718877d
Sandro Schmid <saseb.schmid@gmail.com>
2018-08-01 18:07:05 +0200

    use strings as enum-values

--- src/model/log-mode.enum.ts
+++ src/model/log-mode.enum.ts
@@ -1,5 +1,5 @@
 export enum LogMode {
-  Disabled = 0, // disable logging
-  Simple = 1, // `LogEntry<?>` except `item`-field
-  Full = 2 // include `item`-field
+  Disabled = 'disabled', // disable logging
+  Simple = 'simple', // `LogEntry<?>` except `item`-field
+  Full = 'full' // include `item`-field
 }
